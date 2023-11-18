1716912781522b3c6ab25e8473a588114864cac1
KAMRUDDIN KHAN. <kamruddin.khan@utkallabs.com>
2023-11-18 18:15:46 +0530

    Class Codes Day 4 Updated

---
+++ HTML/script.js
@@ -0,0 +1,41 @@
+// let vs var vs const
+
+
+// var a =10;
+
+// a="Sample";
+
+// a=[2,7,6,4]
+
+// console.log("Value Of a", a);
+
+// var b = 10;
+
+{
+
+    var c = 20;
+
+    console.log("Inside Block", c)
+
+}
+
+
+// console.log("Outside Block", c)
+
+
+var a = 10;
+
+var a = 20;
+
+let name = "Tom";
+
+let age =26;
+
+let firstName = "";
+
+let lastName = "";
+
+console.log("Value of a =",a)
+
+let greaterNumberOfThisTwoNumbers
+
